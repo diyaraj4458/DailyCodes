81c95d86143356ad32dc1520db044bff173547b8
diyaraj4458 <120264826+diyaraj4458@users.noreply.github.com>
2026-08-30 17:26:05 +0530

    Time: 1259 ms (6.10%) | Memory: 295.7 MB (5.10%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,28 +1,29 @@
-#include <bits/stdc++.h>
-using namespace std;
+//Optimized Approach - O(n^2 logn + nlogn) - o(n^2 logn) time and O(n) space
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        int target = 0;
         sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int j = i + 1, k = nums.size() - 1;
+        set<vector<int>> s;
+        vector<vector<int>> output;
+        for (int i = 0; i < nums.size(); i++){
+            int j = i + 1;
+            int k = nums.size() - 1;
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
-                if (sum < 0) {
+                if (sum == target) {
+                    s.insert({nums[i], nums[j], nums[k]});
                     j++;
-                } else if (sum > 0) {
                     k--;
+                } else if (sum < target) {
+                    j++;
                 } else {
-                    ans.push_back({nums[i], nums[j], nums[k]});
-                    j++;
                     k--;
-                    while (j < k && nums[j] == nums[j - 1]) j++;
-                    while (j < k && nums[k] == nums[k + 1]) k--;
                 }
             }
         }
-        return ans;
+        for(auto triplets : s)
+            output.push_back(triplets);
+        return output;
     }
 };
